13567476c3f7bcf7b96bf7d9b911bdb36ba5bdb4
João Carlos Furtado da Silva <93840976+jaocarlosfurta@users.noreply.github.com>
2025-04-23 13:53:24 -0300

    Otimizado?

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -2,54 +2,50 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
+        int troca = 0;
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                troca = 1;
             }
+        }
+        if (troca == 0){
+            break;
         }
     }
 }
 
-int main() {
-    int Vet[10];
-    int n = 10;
-    
-    srand(time(NULL));
-//--------------------------------------
-    for (int i = 0; i < n; i++){
-
-        Vet[i] = rand() % 101;
-
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("[%d] ", arr[i]);
     }
-//--------------------------------------
-    printf("Vetor desordenado: ");
-
-    for (int i = 0; i < n; i++){
-
-        printf("[%d]  ", Vet[i]);
-
-    }
-//--------------------------------------   
     printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &n);
+
+    int *Vet = (int *)malloc(n * sizeof(int));
+
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        Vet[i] = rand() % 101;
+    }
+
+    printf("Vetor desordenado: ");
+    printArray(Vet, n);
 
     bubbleSort(Vet, n);
-//--------------------------------------
+
     printf("Vetor ordenado: ");
+    printArray(Vet, n);
 
-    for (int i = 0; i < n; i++){
-
-        printf("[%d] ", Vet[i]);
-
-    }
-//--------------------------------------
-
-
-    printf("\n");
-
+    free(Vet);
     return 0;
 }
