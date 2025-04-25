783f7217968fae8de3deb846fa645780baf19555
João Carlos Furtado da Silva <93840976+jaocarlosfurta@users.noreply.github.com>
2025-04-25 11:46:44 -0300

    100% atualizado

--- merge-sort.cpp
+++ merge-sort.cpp
@@ -2,11 +2,88 @@
 #include <stdlib.h>
 #include <time.h>
 
+void merge(int arr[], int left, int mid, int right);
 
-void mergeSort(){
-    
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
 }
 
-int main(){
+void merge(int arr[], int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
 
+    int *L = (int *)malloc(n1 * sizeof(int));
+    int *R = (int *)malloc(n2 * sizeof(int));
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+
+    free(L);
+    free(R);
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("[%d] ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &n);
+
+    int *Vet = (int *)malloc(n * sizeof(int));
+
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        Vet[i] = rand() % 101;
+    }
+
+    printf("Vetor desordenado: ");
+    printArray(Vet, n);
+
+    mergeSort(Vet, 0, n - 1);
+
+    printf("Vetor ordenado: ");
+    printArray(Vet, n);
+
+    free(Vet);
+    return 0;
 }
